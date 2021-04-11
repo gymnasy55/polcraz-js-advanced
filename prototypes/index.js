197aea08165a1c0bf0134ccdcbcc37e20fa70b6a
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-04-11 16:03:07 +0300

    prototypes

---
+++ prototypes/index.js
@@ -0,0 +1,20 @@
+const person = new Object({
+  name: 'Maxim',
+  age: 25,
+  greet: function () {
+    console.log('Greet!')
+  }
+})
+
+Object.prototype.sayHello = function () {
+  console.log('Hello')
+}
+
+const lena = Object.create(person)
+lena.name = 'Elena'
+
+console.log(lena)
+
+const str = new String('I am string')
+
+console.log(st)
